e72912af89013c0545a461946485b9f6606e0d08
ryann2222 <90405586+ryann2222@users.noreply.github.com>
2021-09-18 18:08:08 -0400

    everything done except for save function

--- Intro.java
+++ Intro.java
@@ -3,18 +3,15 @@ package clickerGame;
 import java.awt.Color;
 import java.awt.Dimension;
 
-import javax.swing.JFrame;
-import javax.swing.JButton;
+import javax.swing.*;
 import java.awt.BorderLayout;
-import javax.swing.JTextField;
-import javax.swing.JLabel;
 import java.awt.Font;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 public class Intro {
 	
-	private static JTextField txtDescription;
+	private static JTextArea txtDescription;
 	
 	/**
 	 * @wbp.parser.entryPoint
@@ -35,7 +32,7 @@ public class Intro {
 		frameIntro.setLocationRelativeTo(null);
 		frameIntro.getContentPane().setLayout(null);
 		
-		JButton btnNewButton = new JButton("Start a Game!");
+		JButton btnNewButton = new JButton("Start the Game!");
 		btnNewButton.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -46,16 +43,20 @@ public class Intro {
 		btnNewButton.setBackground(new Color(165, 42, 42));
 		btnNewButton.setForeground(Color.WHITE);
 		btnNewButton.setFont(new Font("Arial", Font.BOLD, 30));
-		btnNewButton.setBounds(308, 476, 249, 90);
+		btnNewButton.setBounds(308, 476, 280, 90);
 		frameIntro.getContentPane().add(btnNewButton);
 		
-		txtDescription = new JTextField();
-		txtDescription.setFont(new Font("Arial", Font.PLAIN, 13));
+		txtDescription = new JTextArea();
+		txtDescription.setFont(new Font("Arial", Font.PLAIN, 30));
 		txtDescription.setEditable(false);
 		txtDescription.setBounds(12, 116, 832, 350);
 		frameIntro.getContentPane().add(txtDescription);
 		txtDescription.setColumns(10);
-		
+		txtDescription.setText("You’re a lonely boat programmer drifting out in a sea of terrible\r\nspaghetti code " +
+				"and you’ve taken upon yourself a mission that \r\nwould make any other boat shudder: fix the mess around you," +
+				"\r\none line at a time, and code your way back to society.\r\n" +
+				"Perhaps you’ll even make your way to something called Hack\r\nthe North, an " +
+				"mysterious event you know only from the echoing\r\nvoices of the command line….");
 		JLabel lblGameName = new JLabel("KEYBOARD CLICKER");
 		lblGameName.setFont(new Font("Arial", Font.BOLD, 50));
 		lblGameName.setBounds(44, 10, 756, 90);
